79f0d08d04f154e6dbc395071b6c3ba994c87e59
pika <enzi@braindead.io>
2025-11-09 11:05:15 -0600

    chore: updates

--- web/src/js/text-to-speech-widget.ts
+++ web/src/js/text-to-speech-widget.ts
@@ -10,6 +10,8 @@ interface TtsOptions {
   voice?: number,
 }
 
+const LOCALSTORAGE_DEFAULT_VOICE = 'defaultVoice';
+
 @customElement('text-to-speech-widget')
 export class TextToSpeechWidget extends LitElement {
   static styles = [styles];
@@ -20,58 +22,84 @@ export class TextToSpeechWidget extends LitElement {
   voices: SpeechSynthesisVoice[] = window.speechSynthesis.getVoices();
 
   @property()
-  defaultVoiceIdx?: number;
+  voiceIdx: number | null;
 
   ttsQueue: [string, TtsOptions][] = [];
-  speech?: SpeechSynthesisUtterance;
   speaking: boolean = false;
   voiceSet: boolean = false;
 
   constructor() {
     super();
 
-    this.speech = new SpeechSynthesisUtterance();
-    this.speech.rate = 1;
-    this.speech.pitch = 1;
-    this.speech.volume = 1;
-    this.speech.onend = this.speech.onerror = () => {
-      window.speechSynthesis.cancel();
-      this.speaking = false;
-      this.startTtsQueue();
-    }
-    this.speech.onpause = () => {
-      window.speechSynthesis.cancel();
-      this.speaking = false;
-      this.startTtsQueue();
+    try {
+      this.voiceIdx = parseInt(window.localStorage.getItem(LOCALSTORAGE_DEFAULT_VOICE));
+
+      console.log('Reselecting voice', this.voiceIdx);
+    } catch (_) {
+      console.warn('Could not restore previous voice.')
     }
   }
 
-  startTtsQueue() {
+  startTtsQueue = () => {
     if (this.speaking || !this.ttsQueue.length) {
       return;
     }
+
     this.speaking = true;
+    console.log('Speaking...');
+
+    const speech = new SpeechSynthesisUtterance();
+    speech.rate = 1;
+    speech.pitch = 1;
+    speech.volume = 1;
+
+    speech.onend = speech.onerror = () => {
+      window.speechSynthesis.cancel();
+      this.speaking = false;
+      console.log('Done speaking...');
+      this.startTtsQueue();
+    };
+
+    speech.onpause = () => {
+      window.speechSynthesis.cancel();
+      this.speaking = false;
+      console.log('Done speaking...');
+      this.startTtsQueue();
+    };
 
     const [text, options] = this.ttsQueue.shift();
-    this.speech.text = text;
-    this.speech.voice = this.voices[options?.voice || this.defaultVoiceIdx];
-    console.log('saying', text);
-    window.speechSynthesis.speak(this.speech);
+    speech.text = text;
+    speech.voice = this.voices[options?.voice || this.voiceIdx];
+
+    console.log('speech', speech);
+
+    let dummySpeech = new SpeechSynthesisUtterance('.');
+    dummySpeech.onend = () => {
+      console.log('saying', text);
+
+      setTimeout(() => {
+        window.speechSynthesis.speak(speech);
+      }, 3000);
+    };
+
+    window.speechSynthesis.speak(dummySpeech);
   }
 
-  queueTts(text: string[], options: TtsOptions = {}) {
+  queueTts = (text: string[], options: TtsOptions = {}) => {
     this.ttsQueue = this.ttsQueue.concat(text.map((value) => {
       return [value, options];
     }));
+
     this.startTtsQueue();
-  }
+  };
 
   handleVoicesChanged = () => {
     this.voices = window.speechSynthesis.getVoices();
-    if (this.defaultVoiceIdx === undefined && this.voices.length) {
-      this.defaultVoiceIdx = 0;
+
+    if (this.voiceIdx === undefined && this.voices.length) {
+      this.voiceIdx = 0;
     }
-  }
+  };
 
   handleMessage = (msg: MessageEvent) => {
     const { tts, body } = msg;
@@ -79,33 +107,40 @@ export class TextToSpeechWidget extends LitElement {
     if (tts && body) {
       this.queueTts(body.match(/.{1,40}[^ ]*( |$)/g) as string[], {});
     }
-  }
+  };
 
   handleVoiceChange = (evt: InputEvent) => {
     const { target: { value = undefined } = {} } = evt as any;
+
     if (value) {
-      this.defaultVoiceIdx = parseInt(value, 10);
+      this.voiceIdx = parseInt(value, 10);
+
+      window.localStorage.setItem(LOCALSTORAGE_DEFAULT_VOICE, value);
     }
-  }
+  };
 
   connectedCallback() {
     super.connectedCallback();
+
     this.service.on('message', this.handleMessage);
     window.speechSynthesis.addEventListener('voiceschanged', this.handleVoicesChanged);
   }
 
   disconnectedCallback() {
     super.disconnectedCallback();
+
     this.service.off('message', this.handleMessage);
     window.speechSynthesis.removeEventListener('voiceschanged', this.handleVoicesChanged);
   }
 
   render() {
+    const { voiceIdx, handleVoiceChange } = this;
+
     return html`
       <div>
         <div>
           <select
-            _eventChange=${this.handleVoiceChange}
+            @change=${handleVoiceChange}
           >
             ${
               this.voices.map((voice, i) => {
@@ -113,7 +148,8 @@ export class TextToSpeechWidget extends LitElement {
                 return html`
                   <option
                     value=${i}
-                  >${name}${voice.default ? ' (Default)' : ''}</option>
+                    .selected=${i === voiceIdx}
+                  >${name}${i === voiceIdx ? ' (Default)' : ''}</option>
                 `;
               })
             }
